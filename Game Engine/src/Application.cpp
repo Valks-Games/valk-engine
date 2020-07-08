ba4112b1104fd458dbd90843964571eac66fbe1e
valkyrienyanko <sebastianbelle074@gmail.com>
2020-07-08 18:45:47 -0500

    Shader Abstraction

--- Game Engine/src/Application.cpp
+++ Game Engine/src/Application.cpp
@@ -2,8 +2,47 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 
 using namespace std;
+
+struct ShaderProgramSource 
+{
+	string VertexSource;
+	string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const string& filepath)
+{
+	ifstream stream(filepath);
+
+	enum class ShaderType 
+	{
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+
+	string line;
+	stringstream ss[2];
+	ShaderType type = ShaderType::NONE;
+	while (getline(stream, line)) 
+	{
+		if (line.find("#shader") != string::npos) 
+		{
+			if (line.find("vertex") != string::npos) 
+				type = ShaderType::VERTEX;
+			else if (line.find("fragment") != string::npos)
+				type = ShaderType::FRAGMENT;
+		}
+		else 
+		{
+			ss[(int)type] << line << '\n';
+		}
+	}
+
+	return { ss[0].str(), ss[1].str() };
+}
 
 static unsigned int CompileShader(unsigned int type, const string& source)
 {
@@ -55,7 +94,7 @@ int main(void)
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(640, 480, "Game Engine", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -71,9 +110,9 @@ int main(void)
 	cout << glGetString(GL_VERSION) << endl;
 
 	float vertices[] = {
-		-0.5f, 0.0f,
-		 0.0f, 0.5f,
-		 0.5f, 0.0f
+		-0.5f, -0.5f,
+		 0.0f,  0.5f,
+		 0.5f, -0.5f
 	};
 
 	unsigned int vbo;
@@ -84,27 +123,9 @@ int main(void)
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
-	string vertexShader =
-		"#version 330 core\n"
-		"\n"
-		"layout(location = 0) in vec4 position;\n"
-		"\n"
-		"void main()\n"
-		"{\n"
-		"	gl_Position = position;\n"
-		"}\n";
+	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
 
-	string fragmentShader =
-		"#version 330 core\n"
-		"\n"
-		"layout(location = 0) out vec4 color;\n"
-		"\n"
-		"void main()\n"
-		"{\n"
-		"	color = vec4(1.0, 0.0, 0.0, 1.0);\n"
-		"}\n";
-
-	unsigned int shader = CreateShader(vertexShader, fragmentShader);
+	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
 	glUseProgram(shader);
 
 	/* Loop until the user closes the window */
